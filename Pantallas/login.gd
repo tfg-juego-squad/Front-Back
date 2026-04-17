c9c04e6991d1370e858369ca05b3f2a24d02af08
aquesada <alejandro.quesada@ubunet.es>
2026-04-17 12:59:09 +0200

    feat: Refactorizacion UI, HUD Alumno, Menu de Pausa y Sistema de Notificaciones

--- Pantallas/login.gd
+++ Pantallas/login.gd
@@ -17,25 +17,19 @@ func _on_login_pressed():
 	
 	# Validación básica
 	if usuario.is_empty() or contrasena.is_empty():
-		mensaje_error.text = "Rellena todos los campos"
+		Notificador.notificar("Rellena todos los campos", Color.MAGENTA)
 		return
 	
-	mensaje_error.text = "Conectando..."
+	Notificador.notificar("Autenticando usuario...", Color.CYAN)
 	
 	# TODO: Conectar con el backend real (GET /tfg/usuarios/nombre/{nombre})
 	# Por ahora simulamos el login
 	_simular_login(usuario, contrasena)
 
 func _simular_login(usuario: String, _contrasena: String):
-	# Simulación: si el nombre contiene "profe" → es profesor
-	# En producción, esto consultaría al backend
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(1.0).timeout
 	
 	if usuario.to_lower().find("profe") != -1 or usuario.to_lower() == "admin":
-		# Es profesor → ir al dashboard del profesor
-		mensaje_error.text = ""
 		get_tree().change_scene_to_file("res://Pantallas/profesor_dashboard.tscn")
 	else:
-		# Es alumno → ir al juego (nivel)
-		mensaje_error.text = ""
 		get_tree().change_scene_to_file("res://Niveles/nivel_01.tscn")
