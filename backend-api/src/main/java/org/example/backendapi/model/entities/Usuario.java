a8a9923b57a6b5c616ea94794420acc821a137d3
daaviddieeguez <daaviddieeguez@gmail.com>
2026-05-06 14:14:19 +0200

    feat: implementado UserDetails en Usuario para que Spring Security entienda lo que es un Usuario

--- backend-api/src/main/java/org/example/backendapi/model/entities/Usuario.java
+++ backend-api/src/main/java/org/example/backendapi/model/entities/Usuario.java
@@ -3,13 +3,18 @@ package org.example.backendapi.model.entities;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.ColumnDefault;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -57,4 +62,33 @@ public class Usuario {
             this.experiencia = this.experiencia % xpRequeridaPorNivel;
         }
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Transformamos el enum TipoRol (ROL_PROFESOR o ROL_ESTUDIANTE)
+        // en un objeto de autoridad que Spring Security entienda.
+        return List.of(new SimpleGrantedAuthority(this.rol.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.hashContrasena;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.nombreUsuario;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 }
